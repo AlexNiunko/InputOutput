cb0b49b71b737d5fda885686a2b31155adc09445
AlexNiunko <alexniunko89@gmail.com>
2022-09-12 22:43:56 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javafx.scene.transform.Scale;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s1=scanner.next();
+        String s2=scanner.next();
+        String s3=scanner.next();
+        System.out.println("Длинна первой строки равна"+s1.length());
+        System.out.printf("Длинна второй строки = %d \n",s2.length());
+       try( PrintWriter pr = new PrintWriter(System.out)) {
+           pr.println("Длинна третьей строки равна " + s3.length());
+       }
+    }
+}
